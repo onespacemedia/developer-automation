7ce078d6db337dc5452e4a65b4aec841fd03dd09
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-17 23:07:53 +0000

    Add debugging code.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@ from cookiecutter.main import cookiecutter
 
 from getpass import getpass
 import os
+from pprint import pprint
 import sys
 from shutil import rmtree
 
@@ -32,29 +33,46 @@ credentials = {
 }
 
 # Call each of the plugins to get the API keys we need.
-credentials.update(
-    github.Plugin().call(credentials)
-)
+# credentials.update(
+#     github.Plugin().call(credentials)
+# )
 
-credentials.update(
-    mandrill.Plugin().call(credentials)
-)
+# credentials.update(
+#     mandrill.Plugin().call(credentials)
+# )
 
-credentials.update(
-    google.Plugin().call(credentials)
-)
+# credentials.update(
+#     google.Plugin().call(credentials)
+# )
 
-credentials.update(
-    opbeat.Plugin().call(credentials)
-)
+# credentials.update(
+#     opbeat.Plugin().call(credentials)
+# )
 
-print credentials
+# This is a bit weird, but it's the only way to get the data entered into _this_
+# project into the other one.
+credentials['project_name'] = "{{ cookiecutter.project_name }}"
+credentials['domain_name'] = "{{ cookiecutter.domain_name }}"
+credentials['staging_subdomain'] = "{{ cookiecutter.staging_subdomain }}"
+credentials['github_name'] = "{{ cookiecutter.github_name }}"
+credentials['repo_name'] = "{{ cookiecutter.base_repo_name }}"
 
-# Remove the directory we created.
-rmtree(current_path)
+credentials['clone_url'] = "git@github.com:onespacemedia/example-project.git"
+credentials['email_password'] = 'mandrill_password'
+credentials['google_plus_key'] = 'google_plus_key'
+credentials['google_plus_secret'] = 'google_plus_secret'
+credentials['opbeat_app_id'] = 'opbeat_app_id'
+credentials['opbeat_secret_token'] = 'opbeat_secret_token'
 
+pprint(credentials)
+
+print current_path
+print current_path.replace('{{ cookiecutter.repo_name }}', '')
 
 # Generate the project, passing in the API keys we've just obtained.
-# cookiecutter('gh:onespacemedia/project-template', extra_context={
-#     'foo': 'bar'
-# })
+cookiecutter(
+    # 'gh:onespacemedia/project-template',
+    '~/Workspace/project-template/',
+    output_dir=current_path.replace('{{ cookiecutter.repo_name }}', '{{ cookiecutter.base_repo_name }}'),
+    extra_context=credentials
+)
